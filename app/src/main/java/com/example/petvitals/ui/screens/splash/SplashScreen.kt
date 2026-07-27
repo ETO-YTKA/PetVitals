d8eb5d092f6bb6ad806289e6ee1f58979b76f4cb
Meower <bertenev0586@gmail.com>
2026-07-27 18:04:28 +0300

    refactor splash screen ui and vm, update agp

--- app/src/main/java/com/example/petvitals/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/petvitals/ui/screens/splash/SplashScreen.kt
@@ -1,14 +1,19 @@
 package com.example.petvitals.ui.screens.splash
 
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.petvitals.ui.components.ScreenLayout
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.example.petvitals.ui.theme.PetVitalsTheme
 
 @Composable
 fun SplashScreen(
@@ -17,18 +22,35 @@ fun SplashScreen(
     modifier: Modifier = Modifier,
     viewModel: SplashViewModel = hiltViewModel()
 ) {
-    ScreenLayout(modifier = modifier) {
-        CircularProgressIndicator(
-            modifier = Modifier.width(64.dp),
-            color = MaterialTheme.colorScheme.secondary,
-            trackColor = MaterialTheme.colorScheme.surfaceVariant,
-        )
-    }
-
     LaunchedEffect(true) {
         viewModel.onAppStart(
             onNavigateToPets = onNavigateToMainApp,
             onNavigateToLogIn = onNavigateToLogIn
         )
+    }
+
+    SplashScreenContent(modifier = modifier)
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+private fun SplashScreenContent(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            LoadingIndicator(modifier = Modifier.size(100.dp))
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun SplashScreenContentPreview() {
+    PetVitalsTheme {
+        SplashScreenContent()
     }
 }
--- app/src/main/java/com/example/petvitals/ui/screens/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/petvitals/ui/screens/splash/SplashViewModel.kt
@@ -1,14 +1,14 @@
 package com.example.petvitals.ui.screens.splash
 
+import androidx.lifecycle.ViewModel
 import com.example.petvitals.data.service.account.AccountService
-import com.example.petvitals.ui.screens.PetVitalsAppViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     private val accountService: AccountService
-) : PetVitalsAppViewModel() {
+) : ViewModel() {
 
     fun onAppStart(onNavigateToPets: () -> Unit, onNavigateToLogIn: () -> Unit) {
         if (accountService.hasUser() && accountService.isEmailVerified) onNavigateToPets()
